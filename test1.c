8739289fc5452ccbf85456bb1478b2395fbbed5e
unknown <lxx25@lxx-laptop>
2025-03-21 23:31:33 +0800

    Initial commit

---
+++ test1.c
@@ -0,0 +1,5 @@
+#include "test1.h"
+
+int test() {
+    return 1;
+}
---
+++ test2.c
@@ -0,0 +1,10 @@
+#include "test1.h"
+#include <stdio.h>
+
+int main() {
+    int a;
+    a = test();
+    printf("%d",a);
+    getchar();
+    return 0;
+}
